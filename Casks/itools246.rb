410dda0e0b1d8c7dadc7ccf01fe2f922da80d1e1
will <githubutilities@163.com>
2015-04-29 20:22:22 +0800

    add itools version 2.4.6 cask

---
+++ Casks/itools246.rb
@@ -0,0 +1,13 @@
+cask :v1 => 'itools246' do
+  version '2.4.6'
+  sha256 '412dcd5a082ebf88566a4422e4c1d127e063a65ad9d8f3917e0b29e871da6783'
+
+  # itools.hk is the official download host per the vendor homepage
+  url "https://github.com/githubutilities/macBinary/raw/master/Binary/iTools/itools-2.4.6.dmg"
+  name 'iTools'
+  homepage 'http://pro.itools.cn/mac'
+  license :gratis
+
+  app 'iTools.app'
+end
+
