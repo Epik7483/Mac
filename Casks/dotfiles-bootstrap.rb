42a3090ec7a02b61b42666684538b4e3af40a904
will <githubutilities@163.com>
2015-06-04 22:01:13 +0800

    Add dotfiles bootstrap cask

---
+++ Casks/dotfiles-bootstrap.rb
@@ -0,0 +1,14 @@
+cask :v1 => 'dotfiles-bootstrap' do
+  version :latest
+  sha256 :no_check
+
+  url 'https://raw.githubusercontent.com/githubutilities/dotfiles-bootstrap/master/Contents.zip'
+  name 'dotfiles-bootstrap'
+  homepage 'https://github.com/githubutilities/dotfiles-bootstrap'
+  license :unknown
+
+  installer :script => {
+              :executable => 'Contents/bootstrap.sh',
+              :sudo => false
+            }
+end
