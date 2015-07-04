df993bfb91e7f9651dc7b9289ed62951ec4c435b
will <githubutilities@163.com>
2015-07-04 16:20:24 +0800

    Add Alfred v2.7.1_387 cask

---
+++ Casks/alfred27.rb
@@ -0,0 +1,16 @@
+cask :v1 => 'alfred27' do
+  version '2.7.1_387'
+  sha256 'a3738d0513d736918a6d71535ef3d85dd184af267c05698e49ac4c6b48f38e17'
+
+  url "https://raw.githubusercontent.com/githubutilities/macBinary/master/Binary/Alfred/Alfred_#{version}.zip"
+  name 'Alfred'
+  homepage 'http://www.alfredapp.com/'
+  license :freemium
+
+  app 'Alfred 2.app'
+  app 'Alfred 2.app/Contents/Preferences/Alfred Preferences.app'
+
+  postflight do
+    suppress_move_to_applications :key => 'suppressMoveToApplications'
+  end
+end
